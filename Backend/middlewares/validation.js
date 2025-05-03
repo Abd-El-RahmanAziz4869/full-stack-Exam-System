b4b6ff80c565bf78506c2e95c236bd7cf9a83b8b
ayasabry2 <ayasabry410@gmail.com>
2025-05-03 19:03:17 +0300

    Add backend middleware files

---
+++ Backend/middlewares/validation.js
@@ -0,0 +1,11 @@
+exports.validation = (schema) => {
+    return (req, res, next) => {
+      let x = schema.validate({ ...req.body, ...req.params }, { abortEarly: false });
+      
+      if (x.error) {
+        return res.status(422).json({ status: "fail", message: x.error.details });
+      } else {
+        next();
+      }
+    };
+  };
